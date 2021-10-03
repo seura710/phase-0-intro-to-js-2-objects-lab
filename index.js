eb7a7921eaf7484bc370f722750a916ac44adf4a
Harris Kim <seura710@gmail.com>
2021-10-03 00:49:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "Jane",
+    streetAddress: "Tenafly Road",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+  
+    newEmployee[key] = value;
+  
+    return newEmployee;
+  }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; //Why are we using bracket notation here?
+  
+    return employee;
+  }
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value; //Why are we using bracket notation here?
+  
+    return employee;
+  }
